f2645cc439d19a4236e7c7fef5dd380fd9e8a37e
Bettinelli, Romain <Romain.Bettinelli@evotec.com>
2024-11-20 16:15:22 +0100

    Finish day 4 rock paper scissors

---
+++ beginner_tier/4_rock_paper_scissors.py
@@ -0,0 +1,43 @@
+import random
+player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors\n"))
+
+rockpaperscissors=[
+""" 
+           ______
+       ___,  ____)
+            (_____)
+            (_____)
+            (____)
+       ---.__(__)
+""",
+"""
+        _______
+    ___'   ____)____
+            _________)
+           __________)
+            ________)
+    ---.___________)
+    
+    """
+    ,
+"""
+        _______
+    ___'   ____)____
+              ______)
+           __________)
+          (____)
+    ---.__(___)
+    
+    """
+]
+print(rockpaperscissors[player_choice])
+computer_choice=random.randint(0,2)
+print(f"Computer chose: \n {rockpaperscissors[computer_choice]}")
+
+if computer_choice==player_choice:
+    print("Tie")
+elif computer_choice==player_choice+1 or computer_choice==0 and player_choice==2:
+    print("Computer wins; YOU LOSE")
+else:
+    print("You win!")
+    
