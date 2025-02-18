93a6850ab9f14cf731d7e539ff71a7e18ae0f7b0
Bettinelli, Romain <Romain.Bettinelli@evotec.com>
2025-02-18 09:47:01 +0100

    black password generator

--- beginner_tier/5_password_generator.py
+++ beginner_tier/5_password_generator.py
@@ -1,4 +1,5 @@
 import random
+
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 numbers = [str(number) for number in range(10)]
@@ -6,18 +7,18 @@ letters = [letter for letter in alphabet] + [letter.upper() for letter in alphab
 symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
 print("Welcome to the PyPassword Generator!")
 
-length= int(input("What length do you want your password to be?"))
-symbol_count= int(input("How many non-alphanum symbols do you want?"))
-number_count= int(input("How many numbers do you want?"))
-password=""
-assert(length>=symbol_count+number_count)
+length = int(input("What length do you want your password to be?"))
+symbol_count = int(input("How many non-alphanum symbols do you want?"))
+number_count = int(input("How many numbers do you want?"))
+password = ""
+assert length >= symbol_count + number_count
 for _ in range(number_count):
-    password=password+random.choice(numbers)
+    password = password + random.choice(numbers)
 for _ in range(symbol_count):
-    password=password+random.choice(symbols)
-for _ in range(length-symbol_count-number_count):
-    password=password+random.choice(letters)
-password=list(password)
+    password = password + random.choice(symbols)
+for _ in range(length - symbol_count - number_count):
+    password = password + random.choice(letters)
+password = list(password)
 random.shuffle(password)
 password = "".join(password)
 print(f"Your password is {password}")
