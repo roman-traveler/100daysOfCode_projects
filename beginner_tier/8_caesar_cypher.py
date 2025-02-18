8920c12f04b2b162b03572d03bc21acdbb47ef11
Bettinelli, Romain <Romain.Bettinelli@evotec.com>
2025-02-18 11:20:50 +0100

    day 8: caesar_cypher
    Add decode and swithc

--- beginner_tier/8_caesar_cypher.py
+++ beginner_tier/8_caesar_cypher.py
@@ -1,18 +1,31 @@
-def encrypt_char(char, shift):
+def move_char(char, shift):
+    """
+    Move lower aplhanum char shift positions in table
+    Args: char lowercase ASCII
+    shift: int position shift,can be negative.
+    """
     num = (ord(char)+shift-97)%26 +97
     return chr(num)
+
 def encrypt(message, shift):
     """A Caesar Cypher encryption method"""
-    cypher= [encrypt_char(char, shift) if char.isalpha() else char for char in message]
+    cypher= [move_char(char, shift) if char.isalpha() else char for char in message]
     return "".join(cypher)
 
 def decrypt(message, shift):
     """A Caesar Cypher decryption method"""
-    
+    cypher= [move_char(char, -shift) if char.isalpha() else char for char in message]
+    return "".join(cypher)
     
 method=input("Type encode to encrypt, decode to decrypt\n").lower()
 
 
 text=input("Type your message\n").lower()
 shift = int(input("Type the shift number: \n"))
-print(encrypt(text, shift))
+match method:
+    case "encode":
+        print(f"Here is the encrypted message: {encrypt(text, shift)}")
+    case "decode":
+        print(f"Here is the decrypted message: {decrypt(text, shift)}")
+    case _:
+        print("Invalid method")
