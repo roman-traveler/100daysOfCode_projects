6dd7b203b0a55685957d873aadc087026f13c417
Bettinelli, Romain <Romain.Bettinelli@evotec.com>
2025-02-18 09:46:36 +0100

    Create beginner 5 password generator

---
+++ beginner_tier/5_password_generator.py
@@ -0,0 +1,23 @@
+import random
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+numbers = [str(number) for number in range(10)]
+letters = [letter for letter in alphabet] + [letter.upper() for letter in alphabet]
+symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
+print("Welcome to the PyPassword Generator!")
+
+length= int(input("What length do you want your password to be?"))
+symbol_count= int(input("How many non-alphanum symbols do you want?"))
+number_count= int(input("How many numbers do you want?"))
+password=""
+assert(length>=symbol_count+number_count)
+for _ in range(number_count):
+    password=password+random.choice(numbers)
+for _ in range(symbol_count):
+    password=password+random.choice(symbols)
+for _ in range(length-symbol_count-number_count):
+    password=password+random.choice(letters)
+password=list(password)
+random.shuffle(password)
+password = "".join(password)
+print(f"Your password is {password}")
