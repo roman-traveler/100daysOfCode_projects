de6e969187d56d275f5e239ad3db790c5d5652be
Bettinelli, Romain <Romain.Bettinelli@evotec.com>
2025-02-20 10:04:04 +0100

    day 7: complete hangman

--- beginner_tier/7_hangman.py
+++ beginner_tier/7_hangman.py
@@ -1,8 +1,5 @@
-
-
-
-hangman_array=[
-"""
+hangman_array = [
+    r"""
   ____
   |  |
      |
@@ -11,7 +8,7 @@ hangman_array=[
      |
 ---------
 """,
-"""
+    r"""
   ____
   |  |
   O  |
@@ -20,7 +17,7 @@ hangman_array=[
      |
 ---------
 """,
-"""
+    r"""
   ____
   |  |
   O  |
@@ -29,7 +26,7 @@ hangman_array=[
      |
 ---------
 """,
-""""
+    r"""
   ____
   |  |
   O  |
@@ -38,7 +35,7 @@ hangman_array=[
      |
 ---------
 """,
-"""
+    r"""
   ____
   |  |
   O  |
@@ -47,7 +44,7 @@ hangman_array=[
      |
 ---------
 """,
-"""
+    r"""
   ____
   |  |
   O  |
@@ -56,7 +53,7 @@ hangman_array=[
      |
 ---------
 """,
-"""
+    r"""
   ____
   |  |
   O  |
@@ -65,7 +62,7 @@ hangman_array=[
      |
 ---------
 """,
-"""
+    r"""
   ____
   |  |
   O  |
@@ -73,5 +70,38 @@ hangman_array=[
  / \ |
      |
 ---------
-"""
-]
+""",
+][::-1]  # Displaying this way feels more natural, but I would rather have a count down
+
+lives_remaining = 7
+guessed_word = ""
+word_to_find = "placeholder"
+fail_guesses = []
+correct_guesses = []
+print(f"Word: {"_"*len(word_to_find)}")
+print(hangman_array[lives_remaining])
+while lives_remaining and guessed_word != word_to_find:
+    guess = input("Type a letter\n")
+    if len(guess) != 1:
+        print("Invalid guess")
+        continue
+    if guess in fail_guesses or guess in correct_guesses:
+        print("Choose a new letter; guess already made")
+        continue
+    if guess in word_to_find:
+        print("Correct guess")
+        correct_guesses.append(guess)
+    else:
+        print("Incorrect")
+        fail_guesses.append(guess)
+        lives_remaining -= 1
+    print(f"Fail letters: {fail_guesses}")
+    guessed_word = "".join(
+        [letter if letter in correct_guesses else "_" for letter in word_to_find]
+    )
+    print(f"Word: {guessed_word}")
+    print(hangman_array[lives_remaining])
+if not lives_remaining:
+    print("Game Over")
+else:
+    print("You won!")
