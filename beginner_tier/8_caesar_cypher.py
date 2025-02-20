ebc4d216dab14d85581cdb622665b9e294530dae
Bettinelli, Romain <Romain.Bettinelli@evotec.com>
2025-02-20 10:04:13 +0100

    day 8: cleanup cypher

--- beginner_tier/8_caesar_cypher.py
+++ beginner_tier/8_caesar_cypher.py
@@ -4,23 +4,26 @@ def move_char(char, shift):
     Args: char lowercase ASCII
     shift: int position shift,can be negative.
     """
-    num = (ord(char)+shift-97)%26 +97
+    num = (ord(char) + shift - 97) % 26 + 97
     return chr(num)
+
 
 def encrypt(message, shift):
     """A Caesar Cypher encryption method"""
-    cypher= [move_char(char, shift) if char.isalpha() else char for char in message]
+    cypher = [move_char(char, shift) if char.isalpha() else char for char in message]
     return "".join(cypher)
+
 
 def decrypt(message, shift):
     """A Caesar Cypher decryption method"""
-    cypher= [move_char(char, -shift) if char.isalpha() else char for char in message]
+    cypher = [move_char(char, -shift) if char.isalpha() else char for char in message]
     return "".join(cypher)
-    
-method=input("Type encode to encrypt, decode to decrypt\n").lower()
 
 
-text=input("Type your message\n").lower()
+method = input("Type encode to encrypt, decode to decrypt\n").lower()
+
+
+text = input("Type your message\n").lower()
 shift = int(input("Type the shift number: \n"))
 match method:
     case "encode":
